2447ab83c46820cef9e87ac2f348ace3a7e83d37
Mariusz Gamrat <85711317+JusticeBeaverr@users.noreply.github.com>
2021-10-19 19:45:54 +0200

    Create calc.h

---
+++ SimpleCalculator/calc.h
@@ -0,0 +1,18 @@
+#ifndef CALC_CALC_H
+#define CALC_CALC_H
+
+#endif //CALC_CALC_H
+#ifndef calc
+#define calc
+
+
+int sprawdzInput(int counter, char* argumenty[]);
+
+void Calculate(int wybor , char* argv[]);
+
+void Add(char* argv[]);
+
+void Substract(char* argv[]);
+
+void Volume(char* argv[]);
+#endif

675bac50d6d5e6fdb7f09df9d49a25291a5a1f39
Mariusz Gamrat <85711317+JusticeBeaverr@users.noreply.github.com>
2021-11-01 22:25:27 +0100

    Create main.cpp

---
+++ Lab4/Zad11/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Figura {
+
+public:
+
+   int wiercholki;
+   int *x, *y;
+   Figura(int wiercholki);
+
+};
+
+Figura::Figura(int wierzcholki)
+        :wiercholki(wierzcholki),
+         x(new int[wierzcholki]),
+         y(new int[wierzcholki])
+{
+    for (int i = 0; i < wierzcholki; ++i)
+    {
+        cout << "Wprowadz wspolrzedne " << i << " wiercholka:\n";
+        cin >> x[i];
+        cin >> y[i];
+    }
+}
+
+int main() {
+    int liczba;
+    cout << "Wprowadz ilosc wieszcholkow\n";
+    cin >> liczba;
+    Figura fig(liczba);
+
+    for(int i = 0; i < liczba; i++)
+    {
+        cout << "Wspolrzedne " << i + 1 << " punktu";
+        cout << "(" << fig.x[i] << "," << fig.y[i] << ")\n";
+    }
+    return 0;
+}

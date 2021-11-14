201513320b0ac671ae662fce780fb116ac4221b3
Mariusz Gamrat <85711317+JusticeBeaverr@users.noreply.github.com>
2021-11-14 17:41:45 +0100

    Add files via upload

---
+++ Lab5/Przykład/Examples/E1NoCopyingConstructor/cmake-build-debug/CMakeFiles/e1NoCopyingConstructor.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/fixu2/OneDrive/Pulpit/Lab5/Examples/E1NoCopyingConstructor/src/Point.cpp" "C:/Users/fixu2/OneDrive/Pulpit/Lab5/Examples/E1NoCopyingConstructor/cmake-build-debug/CMakeFiles/e1NoCopyingConstructor.dir/src/Point.cpp.obj"
+  "C:/Users/fixu2/OneDrive/Pulpit/Lab5/Examples/E1NoCopyingConstructor/src/main.cpp" "C:/Users/fixu2/OneDrive/Pulpit/Lab5/Examples/E1NoCopyingConstructor/cmake-build-debug/CMakeFiles/e1NoCopyingConstructor.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

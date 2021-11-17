29ad177827b25d9418605113323877ac91b00fbc
Mariusz Gamrat <85711317+JusticeBeaverr@users.noreply.github.com>
2021-11-17 23:19:58 +0100

    Add files via upload

---
+++ Lab16_11/Vektor/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../include/vector.h"
+//#include "vector.h"
+
+using namespace std;
+
+
+int main() {
+    Vector v1(69,2137), v2(420,77);
+    Vector v3 = v1 + v2;
+    v3 = v1.operator+(v2);
+
+}
+
+
